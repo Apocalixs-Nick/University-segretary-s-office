2b4a924a3433e061e2febd7d028fbd6f83ce36c4
Francesco <francescograuso@live.it>
2022-07-29 11:45:03 +0200

    input main class test, richiesta regex
    regex email funzionante, fai anche password

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Studenti s1 = new Studenti();
+        s1.setName("Francesco");
+        s1.setSurname("Grauso");
+        s1.setUsername("ErCrasher");
+        s1.setEmail("francescograuso@live.it");
+        s1.setCF("GRSFNC02M27M28B");
+
+        System.out.println(s1.getName());
+        System.out.println(s1.getSurname());
+        System.out.println(s1.getUsername());
+        System.out.println(s1.getEmail());
+        System.out.println(s1.getCF());
+
+
+    }
+}
